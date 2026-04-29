dc32bb7cfa79c8dbd6703e579b1d208d08b56d4b
Ethios Solutions <daviandreozzi7@gmail.com>
2026-04-29 20:37:33 -0300

    confere

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -17,10 +17,11 @@ function Taskes({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+            className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${
               task.isCompleted && "line-through"  
             }`}
           >
+            {task.isCompleted && <chechkIcon/> }
             {task.text}
           </button>
 
